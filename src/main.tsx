c5af7a1d9f0346a964b8e3285f578aeab298ae67
Likith Kandepu <withlikith@gmail.com>
2026-06-24 16:22:10 +0530

    added seo

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,27 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import logo2 from './components/logo2.png';
+import { HelmetProvider } from 'react-helmet-async';
 
 function applyFavicon(href: string) {
-	try {
-		let link: HTMLLinkElement | null = document.querySelector('link[rel="icon"]');
-		if (!link) {
-			link = document.createElement('link');
-			link.rel = 'icon';
-			document.head.appendChild(link);
-		}
-		link.type = 'image/png';
-		link.href = href;
-	} catch {}
+  try {
+    let link: HTMLLinkElement | null = document.querySelector('link[rel="icon"]');
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.head.appendChild(link);
+    }
+    link.type = 'image/png';
+    link.href = href;
+  } catch {}
 }
 
 applyFavicon(logo2);
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
 );
